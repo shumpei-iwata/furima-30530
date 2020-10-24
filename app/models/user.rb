0157f91a7b20f0738914bac224b57838d5e03ddb
shumpei-iwata <shn419iwata@icloud.com>
2020-10-24 19:39:10 +0900

    テストコード修正及び不要な記述削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+   
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  
   validates :nickname, presence: true
+  validates :email, presence: true
+  validates :birthday, presence: true
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'please use double-byte characters' } do
-  validates :last_name, presence: true
-  validates :fist_name, presence: true
+    validates :last_name, presence: true
+    validates :fist_name, presence: true
   end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'please use full-width katakana characters' } do
-  validates :last_name_kana, presence: true
-  validates :fist_name_kana, presence: true
+    validates :last_name_kana, presence: true
+    validates :fist_name_kana, presence: true
   end
-  validates :birthday, presence: true
 
   has_many :purchase_records
   has_many :items
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-  nickname       {Faker::Name.initials(number: 2)}
-  email          {Faker::Internet.free_email}
-  password       {Faker::Internet.password(min_length: 6)}
-  # password       {password}
-  password_confirmation {password}
-  last_name      {"田中"}
-  fist_name      {"太郎"}
-  last_name_kana {"タナカ"}
-  fist_name_kana {"タロウ"}
-  birthday      {"2020-4-1"}
+    nickname       { Faker::Name.initials(number: 2) }
+    email          { Faker::Internet.free_email }
+    password       { Faker::Internet.password(min_length: 6)+'1a' }
+    password_confirmation { password }
+    last_name      { '田中' }
+    fist_name      { '太郎' }
+    last_name_kana { 'タナカ' }
+    fist_name_kana { 'タロウ' }
+    birthday { '2020-4-1' }
   end
 end
