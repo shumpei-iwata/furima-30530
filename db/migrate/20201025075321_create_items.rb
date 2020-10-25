77bdf15236cefe65a3a27533fae028c6f2a691d6
shumpei-iwata <shn419iwata@icloud.com>
2020-10-25 18:22:27 +0900

    カラム名の追加

--- db/migrate/20201025075321_create_items.rb
+++ db/migrate/20201025075321_create_items.rb
@@ -1,8 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string  :product_name,        null: false
+      t.text    :description,         null: false
+      t.integer :price,               null: false
+      t.integer :category_id,         null: false
+      t.integer :states_id,           null: false
+      t.integer :delivery_charge_id,  null: false
+      t.integer :area_id,             null: false
+      t.integer :day_id,              null: false
+      t.integer :user_id,             null: false
       t.timestamps
+    
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 ActiveRecord::Schema.define(version: 2020_10_25_075321) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "states_id", null: false
+    t.integer "delivery_charge_id", null: false
+    t.integer "area_id", null: false
+    t.integer "day_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
