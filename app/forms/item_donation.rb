8877b308d508ae36d604619ad5dfeaf7bf6471ec
shumpei-iwata <shn419iwata@icloud.com>
2020-10-28 19:30:24 +0900

    ファイル修正

--- app/forms/item_donation.rb
+++ app/forms/item_donation.rb
@@ -3,5 +3,5 @@ class ItemDonation
   attr_accessor :image, :area_id, :day_id, :category_id, :delivery_charge_id, :states_id, :product_name, :description, :price
 
  #商品購入機能で使うのを間違えて先に作ってしまいました
-  end
+  
 end
