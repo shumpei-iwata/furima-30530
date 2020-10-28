fe49ff114790043d4acfdbb117bd372e6187b72f
shumpei-iwata <shn419iwata@icloud.com>
2020-10-28 20:10:06 +0900

    indexアクション記述、不要なファイル削除

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ActiveHash::Base
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category_id
-  belongs_to_active_hash :states_id
-  belongs_to_active_hash :delivery_charge_id
-  belongs_to_active_hash :area_id
-  belongs_to_active_hash :days_id
-end
