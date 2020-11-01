47224a49884914a2e0c54eb76fa08975c0cc35a7
shumpei-iwata <shn419iwata@icloud.com>
2020-11-01 16:17:21 +0900

    テーブル作成

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,8 +1,20 @@
 class PurchaseAddress < ApplicationRecord
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :purchase_record_id
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :area_id 
+    validates :municipality
+    validates :address 
+    validates :building_name
+    validates :phone_number
+    validates :purchase_record_id
+    end 
+  
 
   def save
-    
+
   end
 end
--- db/migrate/20201101051429_create_shipping_addresses.rb
+++ db/migrate/20201101051429_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.references :purchase_record,   null: false, foreign_key: true
+      t.string :postal_code,        null: false
+      t.string :area_id,            null: false
+      t.string :municipality,       null: false
+      t.string :address,            null: false
+      t.string :building_name,      null: false
+      t.string :phone_number,       null: false
       t.timestamps
     end
   end
